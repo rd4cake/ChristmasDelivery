33759a0aa5a4da3900e418d7b34a8a9bb8cd17a0
rd4cake <dennywang2003@gmail.com>
2020-12-25 16:24:10 -0500

    added pickup

---
+++ Game/Assets/Scripts/Pickup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public Transform mPresent;
+    public Transform holder;
+    bool pickup = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject present = GameObject.FindWithTag("Present");
+        if (pickup==true)
+        {
+            present.transform.position = holder.position;
+            Debug.Log(pickup);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Present")
+        {
+            pickup = true;
+        }
+
+    }
+}
