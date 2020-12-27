e7c0e1f8b8fd1e96a0edc34b9d556c0beabc0058
Jacky <jacky.ji12345@gmail.com>
2020-12-27 16:12:30 -0500

    Added new BG music and fixed health bar

--- Game/Assets/Scripts/PlayerDamage.cs
+++ Game/Assets/Scripts/PlayerDamage.cs
@@ -1,32 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerDamage : MonoBehaviour
 {
-    public float mMaxhealth=100;
-    public float mEnemydamage=10;
-    public GameObject mHealthbar;
-    public float mDamagecooldown = 1;
-    float mNextdamage = 0;
-
-    private void Start()
-    {
-        
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Slider mHealthBar;
+    private float mEnemydamage = 0.2f;
+    private float mDamagecooldown = 1;
+    private float mNextdamage = 0;
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            if (mHealthbar.transform.localScale.x > 0 && Time.time> mNextdamage)
+            if (mHealthBar.value > 0 && Time.time> mNextdamage)
             {
-                TakeDamage();
+                mHealthBar.value -= mEnemydamage;
                 mNextdamage = Time.time + mDamagecooldown;
             }
         }
@@ -35,13 +23,7 @@ public class PlayerDamage : MonoBehaviour
     {
         if (collision.gameObject.tag == "Car")
         {
-            mHealthbar.transform.localScale = new Vector2(0, mHealthbar.transform.localScale.y);
+            mHealthBar.value = 0;
         }
-    }
-
-    void TakeDamage()
-    {
-        float damage = mEnemydamage / mMaxhealth;
-        mHealthbar.transform.localScale=new Vector2(mHealthbar.transform.localScale.x - damage, mHealthbar.transform.localScale.y);
     }
 }
