e2e85cf1a61bb7393ae1e8330aa44efbe02be033
rd4cake <dennywang2003@gmail.com>
2020-12-28 16:30:24 -0500

    Revert "fixed enemy spawner"
    This reverts commit 6ae3e101988eade978ea6fc5bd42fce9ccb32d2b.

--- Game/Assets/Scripts/EnemySpawner.cs
+++ Game/Assets/Scripts/EnemySpawner.cs
@@ -1,17 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject[] mEnemy;
+    public GameObject mEnemy;
     public GameObject mPlayer;
     GameObject mPresentcheck;
     public Collider2D[] mColliders;
     public LayerMask mMask;
     bool condition = false;
-    GameObject nNewEnemy;
 
     private void Start()
     {
@@ -22,16 +20,11 @@ public class EnemySpawner : MonoBehaviour
         condition = false;
         while (condition == false)
         {
-
             Vector3 Newpos = new Vector3(Random.Range(-13f, 103f), Random.Range(28f, -60f), 0);
             mColliders = Physics2D.OverlapCircleAll(Newpos, 3, mMask);
             if (mColliders.Length == 0 && (transform.position - mPlayer.transform.position).magnitude > 100)
             {
-                int type = Random.Range(0, 2);
-                GameObject nNewEnemy=Instantiate(mEnemy[type], Newpos, Quaternion.identity);
-                var Enemyspeed = nNewEnemy.GetComponent<AIPath>();
-                float speed= Random.Range(10.0f, 15.0f);
-                Enemyspeed.maxSpeed = speed;
+                Instantiate(mEnemy, Newpos, Quaternion.identity);
                 condition = true;
             }
         }
