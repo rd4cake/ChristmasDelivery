ade3fa8d9bf1eadc6b3dfe8be103f7f7c8f4bf06
rd4cake <dennywang2003@gmail.com>
2020-12-28 16:32:01 -0500

    Revert "added highscore"
    This reverts commit dff498a27e2957f121da2c5958bf57efc5882554.

--- Game/Assets/Scripts/EnemySpawner.cs
+++ Game/Assets/Scripts/EnemySpawner.cs
@@ -22,7 +22,7 @@ public class EnemySpawner : MonoBehaviour
         {
             Vector3 Newpos = new Vector3(Random.Range(-13f, 103f), Random.Range(28f, -60f), 0);
             mColliders = Physics2D.OverlapCircleAll(Newpos, 3, mMask);
-            if (mColliders.Length == 0 && (transform.position - mPlayer.transform.position).magnitude > 100)
+            if (mColliders.Length == 0 && (transform.position - mPlayer.transform.position).magnitude > 10)
             {
                 Instantiate(mEnemy, Newpos, Quaternion.identity);
                 condition = true;
