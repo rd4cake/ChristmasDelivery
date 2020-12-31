13b72f3454fc89510b1d823bc5f23f006685befc
rd4cake <dennywang2003@gmail.com>
2020-12-30 19:06:05 -0500

    added cookie spawner

---
+++ Game/Assets/Scripts/CookieSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieSpawner : MonoBehaviour
+{
+    public GameObject mCookie;
+    public GameObject mPlayer;
+    public Collider2D[] mColliders;
+    public LayerMask mMask;
+    GameObject[] mCookiecount;
+
+    private void Start()
+    {
+        InvokeRepeating("spawntime", 10f, 8f);
+    }
+    void spawntime()
+    {
+        bool condition = false;
+        while (condition == false)
+        {
+            mCookiecount = GameObject.FindGameObjectsWithTag("Cookie");
+            int mCookieamount = mCookiecount.Length;
+            Vector3 Newpos = new Vector3(Random.Range(-13f, 103f), Random.Range(28f, -60f), 0);
+            mColliders = Physics2D.OverlapCircleAll(Newpos, 3, mMask);
+            if (mCookieamount != 3 && mColliders.Length == 0 && (transform.position - mPlayer.transform.position).magnitude > 20)
+            {
+                Instantiate(mCookie, Newpos, Quaternion.identity);
+            }
+            condition = true;
+        }
+    }
+}
---
+++ Game/Assets/Scripts/HealthGain.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthGain : MonoBehaviour
+{
+    private float mAmountgain = 0.201f;
+    public PlayerDamage mPlayerDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPlayerDamage=GameObject.Find("Santa").GetComponent<PlayerDamage>();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            mPlayerDamage.mHealthBar.value += mAmountgain;
+            Destroy(gameObject);
+        }
+    }
+}
