70bc32a95a13cad63d13ff3c38c19b44727ea2a9
Jacky <jacky.ji12345@gmail.com>
2020-12-30 19:32:06 -0500

    Fixed HealthGain merge error

--- Game/Assets/Scripts/HealthGain.cs
+++ Game/Assets/Scripts/HealthGain.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HealthGain : MonoBehaviour
 {
     public PlayerDamage mPlayerDamage;
-    // Start is called before the first frame update
+
     void Start()
     {
         mPlayerDamage=GameObject.Find("Santa").GetComponent<PlayerDamage>();
@@ -14,12 +14,8 @@ public class HealthGain : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-<<<<<<< HEAD
             AudioManager.PlayAudioFile(4);
-            mPlayerDamage.mHealthBar.value += mAmountgain;
-=======
             mPlayerDamage.mHealthBar.value = 1;
->>>>>>> 0deda09cd74f98cf6b76006ace8972100a7c3077
             Destroy(gameObject);
         }
     }
